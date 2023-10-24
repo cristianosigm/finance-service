3a191025014ed2a9aa834014e540eb5ffe100679
Cristiano Souza <cristianojava@gmail.com>
2023-10-24 15:17:24 -0300

    Small change

--- src/main/java/com/csc/financemanagement/controller/v1/RootController.java
+++ src/main/java/com/csc/financemanagement/controller/v1/RootController.java
@@ -5,8 +5,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class RootController implements RootResource {
+
     @Override
     public String getVersion() {
         return "finance-management, version 0.1 ALPHA";
     }
+
 }
